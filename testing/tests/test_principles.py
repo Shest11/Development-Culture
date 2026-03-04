595889e6bc46bd24d74f62c0ab9d752aec6d2f30
ShestM <danilasestakov8@gmail.com>
2026-03-04 16:13:21 +0800

    Added basic principles and first test

---
+++ testing/tests/test_principles.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../src")
+#TODO make it with `pip install -e.`
+from math_demo import add
+
+def test_addition():
+    assert add(2, 2) == 4
+    print("Test BASIC ADDITION")
+
+if __name__ == "main":
+    test_addition()
