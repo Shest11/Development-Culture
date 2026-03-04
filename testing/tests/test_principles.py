7ee1c67b64878da244dde71e8192795f4275ca91
ShestM <danilasestakov8@gmail.com>
2026-03-04 16:33:50 +0800

    Update test_principles added test_addition_overcomplex and test_addition_duplicated

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -11,7 +11,19 @@ def test_addition_with_bug():
     assert add_with_bug(2, 2) == 4, "Function did not return"
     assert add_with_bug(0, 0) == 0
     print("Test BUGGED ADDITION PASSED")
-    assert add_with_bug(6, 7) == 13
+    assert add_with_bug(6, 7) == 13 # will fail here
+    
+def test_addition_duplicated():
+    # is it real good test (relies on absence)
+    assert add(2, 3) == 2 + 3
+    
+def test_addition_overcomplex():
+    for i in range(0, 2**32):
+        for j in range(0, 2**32):
+            assert add(i, j) == sum(i, j)
+            assert add(-i, -j) == sum(-i, -j)
+            assert add(i, j) == sum(i, j)
+            assert add(-i, -j) == sum(-i, -j)
 
 
 if __name__ == "main":
